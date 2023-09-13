d4547efbbadfc687185802a687ba1e551443ab4b
Christian Bouwense <christian@bigeye.com>
2023-09-13 14:48:34 -0400

    removed smitty

--- src/guppy.h
+++ src/guppy.h
@@ -4,23 +4,27 @@
 #include <stdbool.h>
 #include <stdio.h>
 
-/***************************************************************************************************
- ** Public API
- ****************************************************************************************************/
+/*************************************************************************************************\
+|* Public API                                                                                     |
+\*************************************************************************************************/
 
 void guppy_print_bool_array(bool array[], size_t length, const char* array_name);
 void guppy_print_char_array(char array[], size_t length, const char* array_name);
+void guppy_print_float_array(float array[], size_t length, const char* array_name);
 void guppy_print_int_array(int array[], size_t length, const char* array_name);
 
-/***************************************************************************************************
- ** Internal implementation
- ****************************************************************************************************/
+/*************************************************************************************************\
+|* Internal implementation                                                                        |
+\*************************************************************************************************/
 
 void guppy_print_bool_array(bool array[], size_t length, const char* array_name) {
     printf("%s: [", array_name);
     for (size_t i = 0; i < length; i++) {
-        if (array[i]) printf("true");
-        else          printf("false");
+        if (array[i] == true) {
+            printf("true");
+        } else {
+            printf("false");
+        }
 
         if (i < length - 1) printf(", ");
     }
