888df914c7f5392a530d856ce4544188753bf592
Christian Bouwense <christian@bigeye.com>
2023-09-13 17:58:39 -0400

    added slices

--- src/guppy.h
+++ src/guppy.h
@@ -4,10 +4,11 @@
 #include <stdbool.h>
 #include <stdio.h>
 
-/*************************************************************************************************\
-|* Public API                                                                                     |
-\*************************************************************************************************/
+/**************************************************************************************************
+ * Public API                                                                                     *
+ **************************************************************************************************/
 
+// Print full arrays.
 void guppy_print_bool_array(bool array[], size_t length, const char* array_name);
 void guppy_print_char_array(char array[], size_t length, const char* array_name);
 void guppy_print_double_array(double array[], size_t length, const char* array_name);
@@ -15,17 +16,28 @@ void guppy_print_float_array(float array[], size_t length, const char* array_nam
 void guppy_print_int_array(int array[], size_t length, const char* array_name);
 void guppy_print_long_array(long array[], size_t length, const char* array_name);
 
-/*************************************************************************************************\
-|* Internal implementation                                                                        |
-\*************************************************************************************************/
+// Print array slices [start, end).
+void guppy_print_bool_array_slice(bool array[], size_t start, size_t end, const char* array_name);
+void guppy_print_char_array_slice(char array[], size_t start, size_t end, const char* array_name);
+void guppy_print_double_array_slice(double array[], size_t start, size_t end, const char* array_name);
+void guppy_print_float_array_slice(float array[], size_t start, size_t end, const char* array_name);
+void guppy_print_int_array_slice(int array[], size_t start, size_t end, const char* array_name);
+void guppy_print_long_array_slice(long array[], size_t start, size_t end, const char* array_name);
+
+/**************************************************************************************************
+ * Internal implementation                                                                        *
+ **************************************************************************************************/
 
 /*
  * TODO:
- * 1) Slices of arrays.
- * 2) Print arrays of arrays.
- * 3) Print arrays of structs.
- * 4) DRY this up a bit perhaps. Most of this is duplication with different print formats.
+ * - Print arrays of arrays.
+ * - DRY this up a bit perhaps. Most of this is duplication with different print formats.
  */
+
+//--------------------------------------------------------------------------------------------------
+// Print array
+//--------------------------------------------------------------------------------------------------
+
 void guppy_print_bool_array(bool array[], size_t length, const char* array_name) {
     printf("%s: [", array_name);
     for (size_t i = 0; i < length; i++) {
@@ -81,6 +93,69 @@ void guppy_print_long_array(long array[], size_t length, const char* array_name)
     for (size_t i = 0; i < length; i++) {
         printf("%ld", array[i]);
         if (i < length - 1) printf(", ");
+    }
+    printf("]\n");
+}
+
+//--------------------------------------------------------------------------------------------------
+// Print array slice
+//--------------------------------------------------------------------------------------------------
+
+void guppy_print_bool_array_slice(bool array[], size_t start, size_t end, const char* array_name) {
+    printf("%s: [", array_name);
+    for (size_t i = start; i < end; i++) {
+        if (array[i] == true) {
+            printf("true");
+        } else {
+            printf("false");
+        }
+
+        if (i < end - 1) printf(", ");
+    }
+    printf("]\n");
+}
+
+void guppy_print_char_array_slice(char array[], size_t start, size_t end, const char* array_name) {
+    printf("%s: [", array_name);
+    for (size_t i = start; i < end; i++) {
+        printf("%c", array[i]);
+        if (i < end - 1) printf(", ");
+    }
+    printf("]\n");
+}
+
+void guppy_print_double_array_slice(double array[], size_t start, size_t end, const char* array_name) {
+    printf("%s: [", array_name);
+    for (size_t i = start; i < end; i++) {
+        printf("%.17f", array[i]);
+        if (i < end - 1) printf(", ");
+    }
+    printf("]\n");
+}
+
+void guppy_print_float_array_slice(float array[], size_t start, size_t end, const char* array_name) {
+    printf("%s: [", array_name);
+    for (size_t i = start; i < end; i++) {
+        printf("%f", array[i]);
+        if (i < end - 1) printf(", ");
+    }
+    printf("]\n");
+}
+
+void guppy_print_int_array_slice(int array[], size_t start, size_t end, const char* array_name) {
+    printf("%s: [", array_name);
+    for (size_t i = start; i < end; i++) {
+        printf("%d", array[i]);
+        if (i < end - 1) printf(", ");
+    }
+    printf("]\n");
+}
+
+void guppy_print_long_array_slice(long array[], size_t start, size_t end, const char* array_name) {
+    printf("%s: [", array_name);
+    for (size_t i = start; i < end; i++) {
+        printf("%ld", array[i]);
+        if (i < end - 1) printf(", ");
     }
     printf("]\n");
 }
--- test/guppy_test.c
+++ test/guppy_test.c
@@ -5,6 +5,13 @@
 
 int main(void) {
     bool bool_array[] = {true, false, true};
+    char char_array[] = {'a', '1', '$'};
+    double double_array[] = {DBL_MIN, DBL_EPSILON, 0.123456789012345};
+    float float_array[] = {FLT_MIN, 17.380001, FLT_MAX};
+    int int_array[] = {INT_MIN, 1, 7, 38, INT_MAX};
+    long long_array[] = {LONG_MIN, 1, 7, 38, LONG_MAX};
+
+    // Print entire arrays
 
     printf("Expected: empty array: []\n");
     printf("Actual:   ");
@@ -16,31 +23,47 @@ int main(void) {
     guppy_print_bool_array(bool_array, 3, "normal boolean array");
     printf("\n");
 
-    char char_array[] = {'a', '1', '$'};
     printf("Expected: char_array: [a, 1, $]\n");
     printf("Actual:   ");
     guppy_print_char_array(char_array, 3, "char_array");
     printf("\n");
-
-    double double_array[] = {DBL_MIN, DBL_EPSILON, 0.123456789012345};
+    
     printf("Expected: double_array: [0.00000000000000000, 0.00000000000000022, 0.12345678901234500]\n");
     printf("Actual:   ");
     guppy_print_double_array(double_array, 3, "double_array");
     printf("\n");
-
-    float float_array[] = {FLT_MIN, 17.380001, FLT_MAX};
+    
     printf("Expected: float_array: [0.000000, 17.380001, 340282346638528859811704183484516925440.000000]\n");
     printf("Actual:   ");
     guppy_print_float_array(float_array, 3, "float_array");
     printf("\n");
 
-    int int_array[] = {INT_MIN, 1, 7, 38, INT_MAX};
     printf("Expected: int_array: [-2147483648, 1, 7, 38, 2147483647]\n");
     printf("Actual:   ");
     guppy_print_int_array(int_array, 5, "int_array");
     printf("\n");
 
-    // int long_array[] = {123412341234123412341234, 2, 3};
+    printf("Expected: long_array: [-9223372036854775808, 1, 7, 38, 9223372036854775807]\n");
+    printf("Actual:   ");
+    guppy_print_long_array(long_array, 5, "long_array");
+    printf("\n");
+
+    // Print array slices
+
+    printf("Expected: empty array: []\n");
+    printf("Actual:   ");
+    guppy_print_bool_array_slice(bool_array, 0, 0, "empty array");
+    printf("\n");
+
+    printf("Expected: normal boolean array: [false, true]\n");
+    printf("Actual:   ");
+    guppy_print_bool_array_slice(bool_array, 1, 3, "normal boolean array");
+    printf("\n");
+
+    printf("Expected: out of bounds boolean array: []\n");
+    printf("Actual:   ");
+    guppy_print_bool_array_slice(bool_array, 3, 3, "out of bounds boolean array");
+    printf("\n");
 
     return 0;
 }
