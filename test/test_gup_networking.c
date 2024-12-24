e732609834023fcb4a834c2204ebb602fd73d510
Christian Bouwense <christian@bigeye.com>
2024-12-24 16:55:55 -0500

    real ips

--- test/test_gup_networking.c
+++ test/test_gup_networking.c
@@ -1,19 +1,21 @@
 #include "../src/guppy_networking.h"
 
+#define SENDER_ADDR "192.168.0.206"
 #define SENDER_PORT 1337
+#define RECEIVER_ADDR "192.168.0.42"
 #define RECEIVER_PORT 1338
 
 int main(int argc, char **argv) {
   if (strcmp(argv[1], "sender") == 0) {
-    GupServer server = gup_server_udp_create("127.0.0.1", SENDER_PORT);
+    GupServer server = gup_server_udp_create(SENDER_ADDR, SENDER_PORT);
 
-    gup_server_udp_send(server, "zoo wee mama", "127.0.0.1", RECEIVER_PORT);
+    gup_server_udp_send(server, "zoo wee mama", RECEIVER_ADDR, RECEIVER_PORT);
 
     gup_server_udp_destroy(server);
   }
 
   if (strcmp(argv[1], "receiver") == 0) {
-    GupServer server = gup_server_udp_create("127.0.0.1", RECEIVER_PORT);
+    GupServer server = gup_server_udp_create(RECEIVER_ADDR, RECEIVER_PORT);
 
     char message[1024] = {0};
     for (int i = 0; i < 3; i++) {
