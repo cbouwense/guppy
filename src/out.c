8317d135bac161178fdad5289debf812593f50f4
Christian Bouwense <cbouwense@proton.me>
2025-06-01 16:16:40 -0400

    append

--- src/out.c
+++ src/out.c
@@ -1,90 +1,71 @@
-#define gup_array_bool_debug(xs) _gup_array_bool_debug(xs, #xs)
-void _gup_array_bool_debug(GupArrayBool* xs, const char* xs_name) {
-    printf("%s: {\n", xs_name);
-    printf("  capacity: %d\n", xs->capacity);
-    printf("  count: %d\n", xs->count);
-    printf("  data: %p\n", (void *)(xs->data));
-    printf("}\n");
+void gup_array_bool_append(GupAllocator* a, GupArrayBool* xs, const bool x) {
+    GUP_RESIZE_ARRAY_IF_NEEDED(a, xs, bool);
+
+    xs->data[xs->count] = x;
+    xs->count++;
 }
 
-#define gup_array_char_debug(xs) _gup_array_char_debug(xs, #xs)
-void _gup_array_char_debug(GupArrayChar* xs, const char* xs_name) {
-    printf("%s: {\n", xs_name);
-    printf("  capacity: %d\n", xs->capacity);
-    printf("  count: %d\n", xs->count);
-    printf("  data: %p\n", (void *)(xs->data));
-    printf("}\n");
+void gup_array_char_append(GupAllocator* a, GupArrayChar* xs, const char x) {
+    GUP_RESIZE_ARRAY_IF_NEEDED(a, xs, char);
+
+    xs->data[xs->count] = x;
+    xs->count++;
 }
 
-#define gup_array_double_debug(xs) _gup_array_double_debug(xs, #xs)
-void _gup_array_double_debug(GupArrayDouble* xs, const char* xs_name) {
-    printf("%s: {\n", xs_name);
-    printf("  capacity: %d\n", xs->capacity);
-    printf("  count: %d\n", xs->count);
-    printf("  data: %p\n", (void *)(xs->data));
-    printf("}\n");
+void gup_array_double_append(GupAllocator* a, GupArrayDouble* xs, const double x) {
+    GUP_RESIZE_ARRAY_IF_NEEDED(a, xs, double);
+
+    xs->data[xs->count] = x;
+    xs->count++;
 }
 
-#define gup_array_float_debug(xs) _gup_array_float_debug(xs, #xs)
-void _gup_array_float_debug(GupArrayFloat* xs, const char* xs_name) {
-    printf("%s: {\n", xs_name);
-    printf("  capacity: %d\n", xs->capacity);
-    printf("  count: %d\n", xs->count);
-    printf("  data: %p\n", (void *)(xs->data));
-    printf("}\n");
+void gup_array_float_append(GupAllocator* a, GupArrayFloat* xs, const float x) {
+    GUP_RESIZE_ARRAY_IF_NEEDED(a, xs, float);
+
+    xs->data[xs->count] = x;
+    xs->count++;
 }
 
-#define gup_array_int_debug(xs) _gup_array_int_debug(xs, #xs)
-void _gup_array_int_debug(GupArrayInt* xs, const char* xs_name) {
-    printf("%s: {\n", xs_name);
-    printf("  capacity: %d\n", xs->capacity);
-    printf("  count: %d\n", xs->count);
-    printf("  data: %p\n", (void *)(xs->data));
-    printf("}\n");
+void gup_array_int_append(GupAllocator* a, GupArrayInt* xs, const int x) {
+    GUP_RESIZE_ARRAY_IF_NEEDED(a, xs, int);
+
+    xs->data[xs->count] = x;
+    xs->count++;
 }
 
-#define gup_array_long_debug(xs) _gup_array_long_debug(xs, #xs)
-void _gup_array_long_debug(GupArrayLong* xs, const char* xs_name) {
-    printf("%s: {\n", xs_name);
-    printf("  capacity: %d\n", xs->capacity);
-    printf("  count: %d\n", xs->count);
-    printf("  data: %p\n", (void *)(xs->data));
-    printf("}\n");
+void gup_array_long_append(GupAllocator* a, GupArrayLong* xs, const long x) {
+    GUP_RESIZE_ARRAY_IF_NEEDED(a, xs, long);
+
+    xs->data[xs->count] = x;
+    xs->count++;
 }
 
-#define gup_array_ptr_debug(xs) _gup_array_ptr_debug(xs, #xs)
-void _gup_array_ptr_debug(GupArrayPtr* xs, const char* xs_name) {
-    printf("%s: {\n", xs_name);
-    printf("  capacity: %d\n", xs->capacity);
-    printf("  count: %d\n", xs->count);
-    printf("  data: %p\n", (void *)(xs->data));
-    printf("}\n");
+void gup_array_ptr_append(GupAllocator* a, GupArrayPtr* xs, const void* x) {
+    GUP_RESIZE_ARRAY_IF_NEEDED(a, xs, void*);
+
+    xs->data[xs->count] = x;
+    xs->count++;
 }
 
-#define gup_array_short_debug(xs) _gup_array_short_debug(xs, #xs)
-void _gup_array_short_debug(GupArrayShort* xs, const char* xs_name) {
-    printf("%s: {\n", xs_name);
-    printf("  capacity: %d\n", xs->capacity);
-    printf("  count: %d\n", xs->count);
-    printf("  data: %p\n", (void *)(xs->data));
-    printf("}\n");
+void gup_array_short_append(GupAllocator* a, GupArrayShort* xs, const short x) {
+    GUP_RESIZE_ARRAY_IF_NEEDED(a, xs, short);
+
+    xs->data[xs->count] = x;
+    xs->count++;
 }
 
-#define gup_array_string_debug(xs) _gup_array_string_debug(xs, #xs)
-void _gup_array_string_debug(GupArrayString* xs, const char* xs_name) {
-    printf("%s: {\n", xs_name);
-    printf("  capacity: %d\n", xs->capacity);
-    printf("  count: %d\n", xs->count);
-    printf("  data: %p\n", (void *)(xs->data));
-    printf("}\n");
+/** Appends the struct, does NOT copy. */
+void gup_array_string_append(GupAllocator* a, GupArrayString* xs, const GupString x) {
+    GUP_RESIZE_ARRAY_IF_NEEDED(a, xs, GupString);
+
+    xs->data[xs->count] = x;
+    xs->count++;
 }
 
-#define gup_array_cstr_debug(xs) _gup_array_cstr_debug(xs, #xs)
-void _gup_array_cstr_debug(GupArrayCstr* xs, const char* xs_name) {
-    printf("%s: {\n", xs_name);
-    printf("  capacity: %d\n", xs->capacity);
-    printf("  count: %d\n", xs->count);
-    printf("  data: %p\n", (void *)(xs->data));
-    printf("}\n");
+void gup_array_cstr_append(GupAllocator* a, GupArrayCstr* xs, const char* x) {
+    GUP_RESIZE_ARRAY_IF_NEEDED(a, xs, char*);
+
+    xs->data[xs->count] = x;
+    xs->count++;
 }
 
